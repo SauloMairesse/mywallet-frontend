5d6730b756fe2166fa19137e63f6199b85704de4
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-05-19 09:30:45 -0300

    Preparando revisão de código

--- src/contexts/userINFO.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const usuarioINFO = createContext();
-
-export default usuarioINFO;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom"
-import React from "react"
-import App from "./components/App"
-import Reset from "./styles/elements/Reset"
-
-ReactDOM.render(<React.StrictMode>
-                    <Reset/>
-                    <App />
-                </React.StrictMode>, document.querySelector('.root'))
